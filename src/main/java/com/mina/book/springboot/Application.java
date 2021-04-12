756bc7c523f49ceb00c465dfaf7a54f1ab0570ad
Mina Lee <wwft09@naver.com>
2021-04-13 00:00:15 +0900

    Chapter 5-1. OAuth2.0(Naver login) & Spring Security

--- src/main/java/com/mina/book/springboot/Application.java
+++ src/main/java/com/mina/book/springboot/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 // 프로젝트 최상단 위치해야 함. 여기부터 설정 읽어감
-@EnableJpaAuditing      // JPA Auditing 활성화
+//@EnableJpaAuditing      // JPA Auditing 활성화
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- src/test/java/com/mina/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/mina/book/springboot/web/HelloControllerTest.java
@@ -1,10 +1,14 @@
 package com.mina.book.springboot.web;
 
+import com.mina.book.springboot.config.auth.SecurityConfig;
 import com.mina.book.springboot.web.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -15,14 +19,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)    // @Service, @Component, @Repository 등은 사용 불가.
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles="USER")
     @Test
-    public void hello_리턴된다() throws Exception {
+    public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
         mvc.perform(get("/hello"))
@@ -30,17 +39,18 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles="USER")
     @Test
-    public void helloDto_리턴된다() throws Exception {
+    public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
         int amount = 1000;
 
-        mvc.perform(get("/hello/dto")
-            .param("name", name)
-            .param("amount", String.valueOf(amount)))
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.name", is(name)))        // $ 기준으로 필드명 명시
-            .andExpect(jsonPath("$.amount", is(amount)));
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
-
 }
